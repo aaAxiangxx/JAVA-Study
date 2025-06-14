6e0d7af613732e696d2c64953fc31f6731c2f1f7
阿翔 <3071055261@qq.com>
2025-06-14 21:20:04 +0800

    //

--- day06-oop/src/extends6constructor/Test3.java
+++ day06-oop/src/extends6constructor/Test3.java
@@ -3,7 +3,6 @@ package extends6constructor;
 public class Test3 {
     //目标：理解this(...)调用兄弟构造器
     //创建对象，存储一个学生的数据
-
     public static void main(String[] args) {
         Student s1 = new Student("张三", '男', 18);
         Student s2 = new Student("李四", '女', 19,"清华大学");
